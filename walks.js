3bc5f1efc54907eb9033f15529b6c968d9611559
AnnaUlyanova <a.ulyanova89@gmail.com>
2016-11-10 12:45:01 +1300

    create full project version 1

---
+++ walks.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  $('#mapForm').change(function() {
+
+    var selectedIsland = $('#mapForm option:selected').val();
+
+    if( selectedIsland == "ALL"){
+      $('a.dot').show(1000);
+    }else {
+      $('a.dot[island*="'+selectedIsland+'"]').show(1000);
+        $('a.dot[island!="'+selectedIsland+'"]').hide(1000);
+    }
+
+  });
+
+    $('a.dot').click(function() {
+      $('a.dot').removeClass('selected');
+      $(this).addClass('selected');
+
+      var walk = '.walk_detail#'+$(this).attr('walk');
+      var htmlCode = $(walk).html();
+
+      $('.detail_container').fadeOut(500, function(){
+        $('.detail_container .walk_detail').html(htmlCode);
+        $('.detail_container').fadeIn(500);
+
+
+      });
+    });
+
+
+});
